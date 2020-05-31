0cff5e26f0a7495b897902c57b13ec200bdb184a
T. Sheng <34408561+tsheng0315@users.noreply.github.com>
2020-05-31 22:44:31 +0100

    Update karatsuba.py

--- karatsuba.py
+++ karatsuba.py
@@ -7,7 +7,7 @@ def karatsuba(x,y):
     else:
         n=max(len(str(x)),len(str(y)))
         ndiv2=math.ceil(n/2)
-        a= x // 10**(ndiv2)  #in large number this int(y / 10**(ndiv2)) would cause lose in precision 
+        a= int(y / 10**(ndiv2)) #in large number this int(y / 10**(ndiv2)) would cause lose in precision 
         #print(a)
         b= x % 10**(ndiv2)
         #print(b)
@@ -27,3 +27,24 @@ def karatsuba(x,y):
         return xy
        
 karatsuba(3141592653589793238462643383279502884197169399375105820974944592,2718281828459045235360287471352662497757247093699959574966967627)
+
+
+## dom's version
+from math import ceil
+def karatsuba(x,y):
+    x_str, y_str = str(x), str(y)
+    if len(x_str)==1 or len(y_str)==1 or len(x_str) != len(y_str):
+        return x*y
+    else:
+        block_length = ceil(len(x_str)/2)
+        a, b = int(x_str[:block_length]), int(x_str[block_length:])
+        c, d = int(y_str[:block_length]), int(y_str[block_length:])
+        return a*c* 10**(2*block_length) + ((a+b)*(c+d)-a*c-b*d)*10**block_length + b*d
+
+a = 3141592653589793238462643383279502884197169399375105820974944592 
+b = 2718281828459045235360287471352662497757247093699959574966967627
+karat = karatsuba(a, b)
+trad = a * b
+print(karat)
+print(trad)
+print(karat == trad)
