12bf54034bb24c16ff2c2e5391f3a141d2490dac
T. Sheng <34408561+tsheng0315@users.noreply.github.com>
2020-06-01 19:35:02 +0100

    Update karatsuba.py

--- karatsuba.py
+++ karatsuba.py
@@ -29,32 +29,33 @@ def karatsuba(x,y):
 karatsuba(3141592653589793238462643383279502884197169399375105820974944592,2718281828459045235360287471352662497757247093699959574966967627)
 
 from math import ceil
-def karatsuba(x,y):
-    x_str,y_str=str(x),str(y)
-    if len(x_str)==1 or len(y_str)==1:
-        return x*y
-    else:
-        block_length=ceil( max(len(x_str)/2,len(y_str)/2)  )
-        
-        a,b = int(x[:block_length]),int(x[block_length:])
-      
-        b= x % 10**(ndiv2)
 
-        c= y // 10**(ndiv2)
+def karatsuba(num1, num2):
+    num1Str = str(num1)
+    num2Str = str(num2)
+    if (num1 < 10) or (num2 < 10):
+        return num1*num2
 
-        d= y % 10**(ndiv2)
+    maxLength = max(len(num1Str), len(num2Str))
+    splitPosition = maxLength / 2
+    high1, low1= int(num1Str[:-splitPosition]), int(num1Str[-splitPosition:])
+    high2, low2= int(num2Str[:-splitPosition]), int(num2Str[-splitPosition:])
+    z0 = karatsuba(low1, low2)
+    z1 = karatsuba((low1 + high1), (low2 + high2))
+    z2 = karatsuba(high1, high2)
 
-        
-        ac=karatsuba(a,c)
-        #print(ac)
-        bd=karatsuba(b,d)
-        #print(bd)
-        prod=karatsuba(a+b,c+d)
-        #print(prod)
-        
-        xy= ac* 10**(2*ndiv2) + (prod-ac-bd)*10**ndiv2 + bd
-        return xy
+    return (z2*10**(2*splitPosition)) + ((z1-z2-z0)*10**(splitPosition))+z0
     
+
+a = 3141592653589793238462643383279502884197169399375105820974944592 
+b = 2718281828459045235360287471352662497757247093699959574966967627
+karat = karatsuba(a, b)
+trad = a * b
+print(karat)
+print(trad)
+print(karat == trad)
+
+#############################################################################################
 
 ## dom's version
 from math import ceil
